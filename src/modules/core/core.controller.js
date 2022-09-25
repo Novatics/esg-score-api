706a2a26498a75acb770057b63d8e5af1e14a99f
Thiago Ferreira <milovaz@gmail.com>
2022-09-25 13:49:22 -0300

    feat: adding customerId param and handling error

--- src/modules/core/core.controller.js
+++ src/modules/core/core.controller.js
@@ -1,8 +1,18 @@
 import coreService from "./core.service";
 
 async function getUserScore(req, res) {
-  const score = await coreService.calculateCustomerScore();
-  res.send(score);
+  const { customerId } = req.params;
+  
+  try {
+    const score = await coreService.calculateCustomerScore(customerId);
+    res.send(score);
+  } catch (e) {
+    if (e.message === 'Customer not found') {
+      res.status(404).send('Customer not found');
+    } else {
+      res.status(400).send('Unknow error');
+    }
+  }
 }
 
 export default {
--- src/modules/core/core.service.js
+++ src/modules/core/core.service.js
@@ -2,9 +2,16 @@ import classifier from "../classifier";
 import scorer from "../scorer";
 import openFinanceClient from '../../clients/openfinance.client';
 
+const defaultOrganizationId = '69665991-da55-4aac-a1f2-32d23daba8fe';
+
 async function calculateCustomerScore(customerId) {
-  const customer = await openFinanceClient.getCustomer('595.080.896-84', '69665991-da55-4aac-a1f2-32d23daba8fe');
-  const transactions = await openFinanceClient.getTransactions('595.080.896-84', '69665991-da55-4aac-a1f2-32d23daba8fe');
+  const customer = await openFinanceClient.getCustomer(customerId, defaultOrganizationId);
+  
+  if (!customer) {
+    throw new Error("Customer not found");
+  }
+
+  const transactions = await openFinanceClient.getTransactions(customerId, defaultOrganizationId);
   const classifiedTransactions = classifier.service.classifyTransactions(transactions);
   const score = scorer.service.scoreClassifiedTransactions(customer, classifiedTransactions);
 
